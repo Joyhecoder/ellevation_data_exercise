8c0f973bbbb3ec707f7a3f24fd508c76fe4fcce5
Joy He <joyhe520@gmail.com>
2023-04-15 22:12:33 -0500

    rewrite missing json to csv function and refactor replace_performance_level_values function

---
+++ function.py
@@ -0,0 +1,12 @@
+def process_data(original_csv_file, temp_csv_file, temp_json_file, result_json_file):
+    #*add a column of id numbers to the csv file
+    add_id(original_csv_file, temp_csv_file)
+    
+    #*convert CSV to JSON 
+    make_json(temp_csv_file, temp_json_file)
+    
+    #* replace the Performance Level values
+    replace_performance_level_values(temp_json_file)
+    
+    #* create a new json file with existing json data:
+    new_json(temp_json_file, result_json_file)
